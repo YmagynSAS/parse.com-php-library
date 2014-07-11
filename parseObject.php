8fc40aed5e5392c733efa5df0a5c1cce3dafb6ce
Mathieu Moriceau <mmoriceau@ymagyn.com>
2014-07-11 12:20:10 +0200

    Add relation.

--- parseObject.php
+++ parseObject.php
@@ -2,7 +2,7 @@
 
 class parseObject extends parseRestClient{
 	public $_includes = array();
-	private $_className = '';
+	public $_className = '';
 
 	public function __construct($class=''){
 		if($class != ''){
@@ -15,34 +15,122 @@ class parseObject extends parseRestClient{
 		parent::__construct();
 	}
 
-	public function __set($name,$value){
-		if($name != '_className'){
+	public function pointer($name, $value) {
+		if (is_array($value)) {
+			$relation = new StdClass();
+			$relation->__op = "AddRelation";
+			foreach ($value as $k => $v) {
+				if (is_object($v) && is_a($v, 'parseObject') && isset($v->data['objectId']) && isset($v->_className)) {
+					$relation->objects[] = array("__type" => "Pointer", "className" => $v->_className, "objectId" => $v->data['objectId']);
+				}
+			}
+			$this->data[$name] = $relation;
+		}
+
+		if (is_object($value) && is_a($value, 'parseObject') && isset($value->data['objectId']) && isset($value->_className)) {
+			$this->data[$name] = array("__type" => "Pointer", "className" => $value->_className, "objectId" => $value->data['objectId']);
+		}
+	}
+
+	public function __set($name, $value) {
+		if($name != '_className') {
 			$this->data[$name] = $value;
 		}
 	}
 
-	public function save(){
+	public function save() {
 		if(count($this->data) > 0 && $this->_className != ''){
-			$request = $this->request(array(
-				'method' => 'POST',
-				'requestUrl' => 'classes/'.$this->_className,
-				'data' => $this->data,
-			));
+			try {
+				$request = $this->request(array(
+					'method' => 'POST',
+					'requestUrl' => 'classes/'.$this->_className,
+					'data' => $this->data,
+				));
+			} catch (Exception $e) {
+				die("An error occured while you trying to save your object:" . $e->getMessage());
+			}
+
+			$this->data['objectId'] = $request->objectId;
+			$this->data['createdAt'] = $request->createdAt;
 			return $request;
 		}
 	}
 
-	public function get($id){
-		if($this->_className != '' || !empty($id)){
-			$request = $this->request(array(
-				'method' => 'GET',
-				'requestUrl' => 'classes/'.$this->_className.'/'.$id
-			));
+	private function getRelation($key, $relation) {
+		$relatedTo = [
+			'object' => [
+				'__type' => 'Pointer',
+				'className' => $this->_className,
+				'objectId' => $this->data['objectId']
+			],
+			'key' => $key
+		];
+		$query = new parseQuery($relation->className);
+		$query->where('$relatedTo', $relatedTo);
+		$resp = $query->find();
+		return $resp;
+	}
+
+	public function linkRelation($key, $include_relation = FALSE) {
+		if (!isset($this->data[$key]))
+			$this->throwError($key . " don't exist");
+		$value = $this->data[$key];
+		if (!isset($value->__type) || (isset($value->__type) && $value->__type != "Relation"))
+			$this->throwError($key . " is not a relation");
 			
-			if(!empty($this->_includes)){
-				$request['include'] = implode(',', $this->_includes);
+		$resp = $this->getRelation($key, $value);
+		$arr = [];
+		foreach ($resp->results as $res) {
+			$arr[] = $this->stdToParse($value->className, $res, $include_relation);
+		}
+		$this->data[$key] = $arr;
+		return $this;
+	}
+
+	private function stdToParse($class, $obj, $include_relation = FALSE) {
+		$objRet = new parseObject($class);
+
+		foreach ($obj as $key => $value) {
+			if (is_object($value) && isset($value->__type) && $value->__type == "Object") {
+				$className = $value->className;
+				unset($value->className);
+				$objRet->{$key} = $this->stdToParse($className, $value, $include_relation);
 			}
-			
+			else if (is_object($value) && isset($value->__type) && $value->__type == "Relation" && $include_relation) {
+				$resp = $this->getRelation($key, $value);
+				$arr = [];
+				foreach ($resp->results as $res) {
+					$arr[] = $this->stdToParse($value->className, $res, $include_relation);
+				}
+				$objRet->{$key} = $arr;
+			}
+			else {
+				$objRet->{$key} = $value;
+			}
+		}
+		return $objRet;
+	}
+
+	public function get($id, $include_relation = FALSE){
+		if($this->_className != '' || !empty($id)) {
+			$urlParams = [];
+			if(!empty($this->_includes)){
+				$urlParams['include'] = implode(',', $this->_includes);
+			}
+
+			try {
+				$request = $this->request(array(
+					'method' => 'GET',
+					'requestUrl' => 'classes/'.$this->_className.'/'.$id,
+					'urlParams' => $urlParams
+				));
+			} catch (Exception $e) {
+				die("An error occured while you trying to get an object:" . $e->getMessage());
+			}
+
+			$this->data['objectId'] = $id;
+			return $this->stdToParse($this->_className, $request, $include_relation);
+
 			return $request;
 		}
 	}
@@ -67,7 +155,6 @@ class parseObject extends parseRestClient{
 		$this->data[$field] = $this->dataType('decrement', $amount);
 	}
 
-
 	public function delete($id){
 		if($this->_className != '' || !empty($id)){
 			$request = $this->request(array(
@@ -79,8 +166,10 @@ class parseObject extends parseRestClient{
 		}		
 	}
 
-	public function addInclude($name){
-		$this->_includes[] = $name;
+	public function addIncludes($name){
+		foreach (func_get_args() as $param) {
+	        $this->_includes[] = $param;
+	    }
 	}
 }
 
